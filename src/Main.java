21021bcf91e3bd798df59f377a1980f5cf9479f2
Hyu <rafael-1440973@educar.rs.gov.br>
2023-06-04 20:24:32 -0300

    Finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static char[][] tabuleiro;
     public static String[] jogadores;
     public static char jogadorAtual;
     public static boolean jogoAcabou;
 
+    //controla o fluxo
     public static void main(String[] args) {
         Inicio.iniciarJogo();
         while (!jogoAcabou) {
@@ -25,21 +25,21 @@ public class Main {
     }
 
     private static void verificarFimDeJogo() {
-        // Verificar linhas
+        // Verifica as linhas
         for (int i = 0; i < 3; i++) {
             if (tabuleiro[i][0] == jogadorAtual && tabuleiro[i][1] == jogadorAtual && tabuleiro[i][2] == jogadorAtual) {
                 jogoAcabou = true;
                 return;
             }
         }
-        // Verificar colunas
+        // Verifica as colunas
         for (int i = 0; i < 3; i++) {
             if (tabuleiro[0][i] == jogadorAtual && tabuleiro[1][i] == jogadorAtual && tabuleiro[2][i] == jogadorAtual) {
                 jogoAcabou = true;
                 return;
             }
         }
-        // Verificar diagonais
+        // Verifica as diagonais
         if ((tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) ||
                 (tabuleiro[0][2] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][0] == jogadorAtual)) {
             jogoAcabou = true;
