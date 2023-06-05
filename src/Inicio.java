c7a9dea83ed0f45644c282b4119e57f18cb25dae
Hyu <rafael-1440973@educar.rs.gov.br>
2023-06-05 14:03:00 -0300

    Bug corrigido

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Random;
 public class Inicio {
 
     public static int modo;
@@ -8,25 +7,24 @@ public class Inicio {
         Main.jogadores = new String[2];
         Main.jogoAcabou = false;
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
         System.out.println("JOGO DA VELHA!\n");
 
         System.out.println("ESCOLHA O MODO DE JOGO:");
-        System.out.println("1 - Jogador vs Máquina");
-        System.out.println("2 - Jogador vs Jogador");
+        System.out.println("1 - Player vs Máquina");
+        System.out.println("2 - Player vs Player");
 
         modo = scanner.nextInt();
         scanner.nextLine();
 
         if (modo == 1) {
-            System.out.println("Digite o nome do primeiro jogador: ");
+            System.out.print("Digite o seu nome, por favor: ");
             Main.jogadores[0] = scanner.nextLine();
             Main.jogadores[1] = "Máquina";
         } else if (modo == 2) {
-            System.out.println("Digite o nome do primeiro jogador:");
+            System.out.print("Digite o nome do primeiro player: ");
             Main.jogadores[0] = scanner.nextLine();
-            System.out.println("Digite o nome do segundo jogador:");
+            System.out.print("Digite o nome do segundo player: ");
             Main.jogadores[1] = scanner.nextLine();
         } else {
             iniciarJogo();
